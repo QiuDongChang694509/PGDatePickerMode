b24bdba30afb87f19d44e877d24f14eca630e49d
qdc <qdc@QDCMacBook-Pro.local>
2018-05-17 11:38:22 +0800

    https://blog.csdn.net/zzqiudc/article/details/80348421

--- PGDatePickerMode/AppDelegate.h
+++ PGDatePickerMode/AppDelegate.h
@@ -4,7 +4,7 @@
 //
 //  Created by qdc on 2018/5/17.
 //  Copyright © 2018年 qdc. All rights reserved.
-//
+////https://blog.csdn.net/zzqiudc/article/details/80348421
 
 #import <UIKit/UIKit.h>
 
--- PGDatePickerMode/ViewController.m
+++ PGDatePickerMode/ViewController.m
@@ -4,8 +4,9 @@
 //
 //  Created by qdc on 2018/5/17.
 //  Copyright © 2018年 qdc. All rights reserved.
+//https://blog.csdn.net/zzqiudc/article/details/80348421
 //
-//
+
 #import "PGDatePickManager.h"
 #import "ViewController.h"
 
