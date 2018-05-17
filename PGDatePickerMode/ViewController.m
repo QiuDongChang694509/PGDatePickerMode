8fbc05c66ed713e289cac97580a6d3a3b21dc235
qdc <qdc@QDCMacBook-Pro.local>
2018-05-17 11:09:20 +0800

    选择控制器

--- PGDatePickerMode/ViewController.m
+++ PGDatePickerMode/ViewController.m
@@ -5,10 +5,11 @@
 //  Created by qdc on 2018/5/17.
 //  Copyright © 2018年 qdc. All rights reserved.
 //
-
+//
+#import "PGDatePickManager.h"
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<PGDatePickerDelegate>
 
 @end
 
@@ -17,7 +18,52 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    UIButton *AddBut= [[UIButton alloc]init];
+    AddBut.frame =CGRectMake(0, 100, 200,50);
+    AddBut.backgroundColor = [UIColor redColor];
+    [self.view addSubview:AddBut];
+    [AddBut addTarget:self action:@selector(TimeButAction) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+    
 }
+
+- (void)TimeButAction
+{
+    PGDatePickManager *datePickManager = [[PGDatePickManager alloc]init];
+    PGDatePicker *datePicker = datePickManager.datePicker;
+    datePicker.delegate = self;
+    datePicker.datePickerMode = PGDatePickerModeDateAndTime;
+    [self presentViewController:datePickManager animated:false completion:nil];
+    
+    NSLog(@"日期");
+}
+
+#pragma mark -选择完时间回调
+- (void)datePicker:(PGDatePicker *)datePicker didSelectDate:(NSDateComponents *)dateComponents {
+    NSLog(@"dateComponents =【 %@】", dateComponents);
+    NSDateComponents * Components =[[NSDateComponents alloc]init];
+    Components =dateComponents;
+    if (Components.month <10) {
+        if (Components.day <10) {
+//            self.ProductModel.text =[NSString stringWithFormat:@"%ld-0%ld-0%ld",(long)Components.year,(long)Components.month,(long)Components.day];
+        }else
+        {
+//            self.ProductModel.text =[NSString stringWithFormat:@"%ld-0%ld-%ld",(long)Components.year,(long)Components.month,(long)Components.day];
+        }
+    }else
+    {
+        if (Components.day <10) {
+//            self.ProductModel.text =[NSString stringWithFormat:@"%ld-%ld-0%ld",(long)Components.year,(long)Components.month,(long)Components.day];
+        }else
+        {
+//            self.ProductModel.text =[NSString stringWithFormat:@"%ld-%ld-%ld",(long)Components.year,(long)Components.month,(long)Components.day];
+        }
+    }
+    
+}
+
 
 
 - (void)didReceiveMemoryWarning {
---
+++ Podfile
@@ -0,0 +1,7 @@
+platform :ios, '8.0'
+#use_frameworks!个别需要用到它，比如reactiveCocoa
+
+target 'PGDatePickerMode' do
+  pod 'AFNetworking', '~> 2.6'
+pod 'PGDatePicker'
+end
